7772fd2131d6cb4af581481bc6981807eec0d9ef
albertocaste <caste_acm@hotmail.com>
2022-03-23 18:33:09 +0100

    Listado de pokemons

--- js/main.js
+++ js/main.js
@@ -1,14 +1,105 @@
-const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+/* Petición HTTP con XHR */
+/* const Http = new XMLHttpRequest();
+const url='https://pokeapi.co/api/v2/pokemon/';
+Http.open("GET", url);
+Http.send();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!!!!');
-});
+Http.onreadystatechange = (e) => {
+  const pokemons = Http.responseText;
+  console.log(pokemons);
+}
+ */
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+/* Petición HTTP con Fetch */
+
+/* Obtener todos los pokemons */
+const getAndPrintPokemons = (url) => {
+    fetch(url, {
+        method: 'GET',
+        redirect: 'follow'
+    })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            let pokemons = result.results;
+            let previous = result.previous;
+            console.log(previous);
+            if(previous == null) {
+                $('#previous').hide();
+            } else {
+                $('#previous').show();
+            }
+            $('#previous').attr('data-url', previous);
+            let next = result.next;
+            $('#next').attr('data-url', next);
+            /* Bucle de pokemons */
+            pokemons.forEach(pokemon => {
+                /* Datos de un pokemon */
+                console.log(pokemon);
+                /* Petición con los detalle de un pokemon */
+                fetch(pokemon.url, {
+                    method: 'GET',
+                    redirect: 'follow'
+                })
+                    .then(response => response.json())
+                    .then(result => {
+                        const pokemon = result;
+                        console.log(pokemon);
+                        let urlImage = pokemon.sprites.front_shiny;
+                        let IdName = pokemon.id + '-' + pokemon.name;
+                        var cardContainer = $('#card-container').clone();
+                       /*  console.log(cardContainer); */
+                        cardContainer.find('.card-title').text(IdName);
+                        cardContainer.find('.card-img-top').attr("src", urlImage);
+                        $('#cards').append(cardContainer);
+                        
+                        /* document.getElementById('card-title').textContent = IdName;
+                        document.getElementById('card-img-top').setAttribute("src", urlImage); */
+                        ///*  */console.log(pokemon);
+                    })
+                    .catch(error => console.log('error', error));
+            });
+    
+    
+            /* for(var i = 0; i < json.length; i++) {
+                var obj = json[i];
+            
+                console.log(obj.id);
+            }
+            pokemons.forEach(pokemon => {
+                console.log(pokemon);
+            }); */
+            console.log(pokemons);
+        })
+        .catch(error => console.log('error', error));
+}
+let url = "https://pokeapi.co/api/v2/pokemon/";
+getAndPrintPokemons(url);
+
+
+$(document).on("click",".page-item", function () {
+    var pageButton = $(this).attr('id'); // or var clickedBtnID = this.id
+    let url = $(this).attr('data-url');
+    $('#cards').html('');
+    getAndPrintPokemons(url);
+ });
+
+
+/* Obtener los detalles de UN pokemon */
+/* fetch("https://pokeapi.co/api/v2/pokemon/1", {
+    method: 'GET',
+    redirect: 'follow'
+})
+    .then(response => response.json())
+    .then(result => {
+        const pokemon = result;
+        let IdName = pokemon.id + '-' + pokemon.name;
+        let urlImage = pokemon.sprites.front_shiny;
+        document.getElementById('card-title').textContent = IdName;
+        document.getElementById('card-img-top').setAttribute("src", urlImage);
+        console.log(pokemon);
+    })
+    .catch(error => console.log('error', error)); */
+
+
